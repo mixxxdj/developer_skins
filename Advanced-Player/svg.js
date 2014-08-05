cc142d8e10af6839dd9b2498adf260050fb355a5
Jean Claveau <jean.claveau@c277.fr>
2014-08-05 17:08:07 +0200

    design of killers

--- Advanced-Player/svg.js
+++ Advanced-Player/svg.js
@@ -42,4 +42,35 @@ templateHooks.prop = prop = function( propName, varName ){
     return '';
 }
 
+
+// http://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+  var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+
+  return {
+    x: centerX + (radius * Math.cos(angleInRadians)),
+    y: centerY + (radius * Math.sin(angleInRadians))
+  };
+}
+
+function describeArc(x, y, radius, startAngle, endAngle){
+
+    var start = polarToCartesian(x, y, radius, endAngle);
+    var end = polarToCartesian(x, y, radius, startAngle);
+
+    var arcSweep = endAngle - startAngle <= 180 ? "0" : "1";
+
+    var d = [
+        "M", start.x, start.y, 
+        "A", radius, radius, 0, arcSweep, 0, end.x, end.y
+    ].join(" ");
+
+    return d;       
+}
+
+/** /
+		<!--path	d="" expr-d="describeArc( 26, 26, 20, -60, 60 )"
+				style="stroke: #cccccc"/-->
+/**/
+
 this.templateHooks.variable = variable;
